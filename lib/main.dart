55c782c4b73500e23f0fd16967c295e915a37a17
leminhtrungsys-del <leminhtrungsys@gmail.com>
2026-07-12 15:35:48 +0700

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'services/auth_service.dart';
+import 'screens/login_screen.dart';
+import 'screens/home_screen.dart';
+import 'utils/constants.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MiniChatApp());
+}
+
+class MiniChatApp extends StatelessWidget {
+  const MiniChatApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Mini Chat',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: AppColors.primary,
+        scaffoldBackgroundColor: AppColors.background,
+        colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primary),
+        useMaterial3: true,
+      ),
+      home: const _RootDecider(),
+    );
+  }
+}
+
+class _RootDecider extends StatefulWidget {
+  const _RootDecider();
+
+  @override
+  State<_RootDecider> createState() => _RootDeciderState();
+}
+
+class _RootDeciderState extends State<_RootDecider> {
+  final _auth = AuthService();
+  bool _loading = true;
+  bool _loggedIn = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _check();
+  }
+
+  Future<void> _check() async {
+    final phone = await _auth.getSessionPhone();
+    setState(() {
+      _loggedIn = phone != null;
+      _loading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_loading) {
+      return const Scaffold(body: Center(child: CircularProgressIndicator()));
+    }
+    return _loggedIn ? const HomeScreen() : const LoginScreen();
+  }
+}
