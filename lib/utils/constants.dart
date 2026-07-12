5be0e561042be87dceb46f630631dbfa1edc137c
leminhtrungsys-del <leminhtrungsys@gmail.com>
2026-07-12 15:36:56 +0700

    Create constants.dart

---
+++ lib/utils/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFF2563EB);
+  static const background = Color(0xFFF7F8FA);
+  static const bubbleMe = Color(0xFF2563EB);
+  static const bubbleOther = Color(0xFFE5E7EB);
+}
